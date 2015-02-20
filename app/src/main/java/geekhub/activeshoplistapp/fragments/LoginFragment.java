448641c0f466be710e58779337b227622aabb30a
Andrew Horishnii <andrew.horishnii@cleverua.com>
2015-02-20 08:26:01 +0200

    add facebook sdk

--- app/src/main/java/geekhub/activeshoplistapp/fragments/LoginFragment.java
+++ app/src/main/java/geekhub/activeshoplistapp/fragments/LoginFragment.java
@@ -1,6 +1,7 @@
 package geekhub.activeshoplistapp.fragments;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
@@ -11,6 +12,11 @@ import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.LoginButton;
+
 import geekhub.activeshoplistapp.R;
 import geekhub.activeshoplistapp.helpers.AppConstants;
 import geekhub.activeshoplistapp.helpers.SharedPrefHelper;
@@ -19,12 +25,22 @@ import geekhub.activeshoplistapp.helpers.SharedPrefHelper;
  * Created by rage on 08.02.15. Create by task: 004
  */
 public class LoginFragment extends BaseFragment{
+    private static final String TAG = "Login Fragment";
     private EditText emailEditText;
     private EditText passwordEditText;
     private CheckBox rememberCheckBox;
     private View loginButton;
     private View continueButton;
     private OnLoginFragmentListener onLoginFragmentListener;
+
+    /*Facebook*/
+    private UiLifecycleHelper uiHelper;
+    private Session.StatusCallback callback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state, Exception exception) {
+            onSessionStateChange(session, state, exception);
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -34,6 +50,9 @@ public class LoginFragment extends BaseFragment{
         rememberCheckBox = (CheckBox) view.findViewById(R.id.checkbox_remember);
         loginButton = view.findViewById(R.id.button_login);
         continueButton = view.findViewById(R.id.button_continue);
+
+        LoginButton facebookAuthButton = (LoginButton) view.findViewById(R.id.button_facebook_auth);
+        facebookAuthButton.setFragment(this);
         return view;
     }
 
@@ -58,6 +77,52 @@ public class LoginFragment extends BaseFragment{
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        uiHelper = new UiLifecycleHelper(getActivity(), callback);
+        uiHelper.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // For scenarios where the main activity is launched and user
+        // session is not null, the session state change notification
+        // may not be triggered. Trigger it if it's open/closed.
+        Session session = Session.getActiveSession();
+        if (session != null &&
+                (session.isOpened() || session.isClosed()) ) {
+            onSessionStateChange(session, session.getState(), null);
+        }
+        uiHelper.onResume();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
+    }
+
+    @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
 
@@ -71,5 +136,13 @@ public class LoginFragment extends BaseFragment{
 
     public interface OnLoginFragmentListener {
         public void onLoginFragmentClickListener(int button);
+    }
+
+    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+        if (state.isOpened()) {
+            Log.i(TAG, "Logged in...");
+        } else if (state.isClosed()) {
+            Log.i(TAG, "Logged out...");
+        }
     }
 }
