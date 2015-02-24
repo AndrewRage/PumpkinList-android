53307f03cd5f94392ea7737019ac530281bcfe0a
Andrew Horishnii <andreyrage@horishniy.org.ua>
2015-02-24 09:29:44 +0200

    create heade view on purchase edit fragment

--- app/src/main/java/geekhub/activeshoplistapp/adapters/PurchaseItemAdapter.java
+++ app/src/main/java/geekhub/activeshoplistapp/adapters/PurchaseItemAdapter.java
@@ -1,17 +1,51 @@
 package geekhub.activeshoplistapp.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
+import geekhub.activeshoplistapp.R;
 import geekhub.activeshoplistapp.model.PurchaseItemModel;
 
 /**
  * Created by rage on 2/22/15.
  */
 public class PurchaseItemAdapter extends ArrayAdapter<PurchaseItemModel> {
+    private Context context;
+    private int resource;
+    private List<PurchaseItemModel> purchaseItemLists;
+    private LayoutInflater inflater;
+
     public PurchaseItemAdapter(Context context, int resource, List<PurchaseItemModel> objects) {
         super(context, resource, objects);
+        this.context = context;
+        this.resource = resource;
+        this.purchaseItemLists = objects;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        PurchaseItemModel purchaseList = purchaseItemLists.get(position);
+        Holder holder;
+        if (convertView == null) {
+            convertView = inflater.inflate(resource, parent, false);
+            holder = new Holder();
+            holder.title = (TextView) convertView.findViewById(R.id.title);
+            convertView.setTag(holder);
+        } else {
+            holder = (Holder) convertView.getTag();
+        }
+        holder.title.setText(purchaseList.getGoodsId() + purchaseList.getGoodsDescription());
+        return convertView;
+    }
+
+    private class Holder {
+        TextView title;
     }
 }
--- app/src/main/java/geekhub/activeshoplistapp/fragments/PurchaseListEditFragment.java
+++ app/src/main/java/geekhub/activeshoplistapp/fragments/PurchaseListEditFragment.java
@@ -5,11 +5,16 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import geekhub.activeshoplistapp.R;
+import geekhub.activeshoplistapp.adapters.PurchaseItemAdapter;
+import geekhub.activeshoplistapp.adapters.PurchaseListAdapter;
+import geekhub.activeshoplistapp.model.PurchaseItemModel;
 
 /**
  * Created by rage on 08.02.15. Create by task: 004
@@ -18,16 +23,28 @@ public class PurchaseListEditFragment extends BaseFragment {
     private static final String TAG = "PurchaseListEditFragment";
     private ListView purchaseListView;
     private View header;
+    private View footer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_purchase_list_edit, container, false);
         purchaseListView = (ListView) view.findViewById(R.id.purchase_item_list);
         header = inflater.inflate(R.layout.purchase_edit_header, purchaseListView, false);
-        ArrayAdapter adapter = new ArrayAdapter<String[]>(view.getContext(), android.R.layout.simple_list_item_1);
-        purchaseListView.addHeaderView(header);
-        purchaseListView.setAdapter(adapter);
+        footer = inflater.inflate(R.layout.purchase_edit_footer, purchaseListView, false);
         return view;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Fake data
+        List<PurchaseItemModel> purchaseItemLists = new ArrayList<>();
+        purchaseItemLists.add(new PurchaseItemModel(false,false,false,1,1,"Desc_str",123));
+
+        PurchaseItemAdapter adapter = new PurchaseItemAdapter(getActivity(), R.layout.purchase_edit_item, purchaseItemLists);;
+        purchaseListView.addHeaderView(header);
+        purchaseListView.addFooterView(footer);
+        purchaseListView.setAdapter(adapter);
+    }
 }
