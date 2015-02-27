ff6217dad396a340d47bc1ebc1864c935084994c
Andrew Horishnii <andreyrage@horishniy.org.ua>
2015-02-27 22:44:37 +0200

    create getter and setter for purchase lists

---
+++ app/src/main/java/geekhub/activeshoplistapp/helpers/DataBaseHelper.java
@@ -0,0 +1,97 @@
+package geekhub.activeshoplistapp.helpers;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import geekhub.activeshoplistapp.model.PurchaseItemModel;
+import geekhub.activeshoplistapp.model.PurchaseListModel;
+
+/**
+ * Created by rage on 2/27/15.
+ */
+public class DataBaseHelper {
+    private static final String LOG = "DataBaseHelper";
+
+    private SqlDbHelper dbHelper;
+    private SQLiteDatabase database;
+
+    public DataBaseHelper(Context context) {
+        dbHelper = SqlDbHelper.getInstance(context);
+    }
+
+    public void open() {
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public long addPurchaseList(long id, PurchaseListModel list) {
+        ContentValues values = new ContentValues();
+        values.put(SqlDbHelper.PURCHASE_LIST_COLUMN_ID, id);
+        values.put(SqlDbHelper.PURCHASE_LIST_COLUMN_LIST_NAME, list.getListName());
+        values.put(SqlDbHelper.PURCHASE_LIST_COLUMN_USER_ID, list.getUserId());
+        values.put(SqlDbHelper.PURCHASE_LIST_COLUMN_SHOP_ID, list.getShopId());
+        values.put(SqlDbHelper.PURCHASE_LIST_COLUMN_TIME_ALARM, list.getTimeAlarm());
+        values.put(SqlDbHelper.PURCHASE_LIST_COLUMN_TIME_CREATE, list.getTimeCreate());
+        values.put(SqlDbHelper.PURCHASE_LIST_COLUMN_TIMESTAMP, list.getTimeStamp());
+        long rawId = database.insert(
+                SqlDbHelper.TABLE_PURCHASE_LIST,
+                null,
+                values
+        );
+        return rawId;
+    }
+
+    public Map<Long,PurchaseListModel> getPurchaseLists(){
+        Map<Long,PurchaseListModel> list = new TreeMap<>();
+        String[] projection = {
+                SqlDbHelper.PURCHASE_LIST_COLUMN_ID,
+                SqlDbHelper.PURCHASE_LIST_COLUMN_LIST_NAME,
+                SqlDbHelper.PURCHASE_LIST_COLUMN_USER_ID,
+                SqlDbHelper.PURCHASE_LIST_COLUMN_SHOP_ID,
+                SqlDbHelper.PURCHASE_LIST_COLUMN_TIME_ALARM,
+                SqlDbHelper.PURCHASE_LIST_COLUMN_TIME_CREATE,
+                SqlDbHelper.PURCHASE_LIST_COLUMN_TIMESTAMP,
+        };
+        String orderBy = SqlDbHelper.PURCHASE_LIST_COLUMN_ID + " DESC";
+        Cursor cursor = database.query(
+                SqlDbHelper.TABLE_PURCHASE_LIST,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                orderBy
+        );
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            int indexId = cursor.getColumnIndex(SqlDbHelper.PURCHASE_LIST_COLUMN_ID);
+            int indexName = cursor.getColumnIndex(SqlDbHelper.PURCHASE_LIST_COLUMN_LIST_NAME);
+            int indexUser = cursor.getColumnIndex(SqlDbHelper.PURCHASE_LIST_COLUMN_USER_ID);
+            int indexShop = cursor.getColumnIndex(SqlDbHelper.PURCHASE_LIST_COLUMN_SHOP_ID);
+            int indexAlarm = cursor.getColumnIndex(SqlDbHelper.PURCHASE_LIST_COLUMN_TIME_ALARM);
+            int indexCreate = cursor.getColumnIndex(SqlDbHelper.PURCHASE_LIST_COLUMN_TIME_CREATE);
+            int indexTimestamp = cursor.getColumnIndex(SqlDbHelper.PURCHASE_LIST_COLUMN_TIMESTAMP);
+            Map<Integer,PurchaseItemModel> purchasesItem = new TreeMap<>();
+            PurchaseListModel listModel = new PurchaseListModel(
+                    cursor.getString(indexName),
+                    cursor.getInt(indexUser),
+                    cursor.getInt(indexShop),
+                    cursor.getLong(indexAlarm),
+                    cursor.getLong(indexCreate),
+                    cursor.getLong(indexTimestamp),
+                    purchasesItem
+            );
+            list.put(cursor.getLong(indexId), listModel);
+            cursor.moveToNext();
+        }
+        return list;
+    }
+}
