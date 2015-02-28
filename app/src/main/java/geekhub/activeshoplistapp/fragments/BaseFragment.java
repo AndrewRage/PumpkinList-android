1d35e430a9fe68e01bba726456811e83075d91d2
Andrew Horishnii <andreyrage@horishniy.org.ua>
2015-02-28 14:30:13 +0200

    add actions create and delete lists

--- app/src/main/java/geekhub/activeshoplistapp/fragments/BaseFragment.java
+++ app/src/main/java/geekhub/activeshoplistapp/fragments/BaseFragment.java
@@ -1,6 +1,8 @@
 package geekhub.activeshoplistapp.fragments;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
+import android.view.inputmethod.InputMethodManager;
 
 /**
  * Created by rage on 06.02.15.
@@ -8,5 +10,12 @@ import android.support.v4.app.Fragment;
  * Base Fragment
  */
 public abstract class BaseFragment extends Fragment {
+
+    public void hideSoftKeyboard() {
+        if(getActivity().getCurrentFocus()!=null) {
+            InputMethodManager inputMethodManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);
+        }
+    }
 
 }
--- app/src/main/java/geekhub/activeshoplistapp/fragments/PurchaseListEditFragment.java
+++ app/src/main/java/geekhub/activeshoplistapp/fragments/PurchaseListEditFragment.java
@@ -1,11 +1,14 @@
 package geekhub.activeshoplistapp.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import geekhub.activeshoplistapp.R;
@@ -24,6 +27,10 @@ public class PurchaseListEditFragment extends BaseFragment {
     private View header;
     private View footer;
     private PurchaseListModel purchaseList;
+    private EditText listNameEdit;
+    private View addNewListButton;
+    private View deleteListButton;
+    private boolean isEdit;
 
     public static PurchaseListEditFragment newInstance(int purchaseListId) {
         PurchaseListEditFragment fragment = new PurchaseListEditFragment();
@@ -43,6 +50,9 @@ public class PurchaseListEditFragment extends BaseFragment {
         purchaseListView = (ListView) view.findViewById(R.id.purchase_item_list);
         header = inflater.inflate(R.layout.purchase_edit_header, purchaseListView, false);
         footer = inflater.inflate(R.layout.purchase_edit_footer, purchaseListView, false);
+        listNameEdit = (EditText) footer.findViewById(R.id.edit_list_name);
+        addNewListButton = footer.findViewById(R.id.button_new_list);
+        deleteListButton = footer.findViewById(R.id.button_delete_list);
         return view;
     }
 
@@ -53,13 +63,40 @@ public class PurchaseListEditFragment extends BaseFragment {
         if (getArguments() != null) {
             int id = getArguments().getInt(ARG_LIST_ID);
             purchaseList = ShoppingHelper.getInstance().getPurchaseLists().get(id);
+            listNameEdit.setText(purchaseList.getListName());
+            addNewListButton.setVisibility(View.GONE);
+            isEdit = true;
         } else {
             purchaseList = new PurchaseListModel();
+            isEdit = false;
         }
 
         PurchaseItemAdapter adapter = new PurchaseItemAdapter(getActivity(), R.layout.purchase_edit_item, purchaseList.getPurchasesItem());;
         purchaseListView.addHeaderView(header);
         purchaseListView.addFooterView(footer);
         purchaseListView.setAdapter(adapter);
+
+        addNewListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideSoftKeyboard();
+                if (TextUtils.isEmpty(listNameEdit.getText())) {
+                    listNameEdit.setText(R.string.purchase_edit_new_list_default);
+                }
+                purchaseList.setListName(listNameEdit.getText().toString());
+                ShoppingHelper.getInstance().getPurchaseLists().add(purchaseList);
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+        deleteListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isEdit) {
+                    ShoppingHelper.getInstance().getPurchaseLists().remove(purchaseList);
+                }
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
     }
 }
