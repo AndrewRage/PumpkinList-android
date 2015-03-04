5286ac116ff15f74f722e0a681141327141669a2
Andrew Horishnii <andreyrage@horishniy.org.ua>
2015-03-04 23:16:00 +0200

    SupportMapFragment

--- app/src/main/java/geekhub/activeshoplistapp/activities/ShopMapActivity.java
+++ app/src/main/java/geekhub/activeshoplistapp/activities/ShopMapActivity.java
@@ -3,6 +3,13 @@ package geekhub.activeshoplistapp.activities;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import geekhub.activeshoplistapp.R;
 import geekhub.activeshoplistapp.helpers.AppConstants;
 import geekhub.activeshoplistapp.helpers.ShoppingHelper;
@@ -11,13 +18,20 @@ import geekhub.activeshoplistapp.model.ShopsModel;
 /**
  * Created by rage on 3/3/15.
  */
-public class ShopMapActivity extends BaseActivity {
+public class ShopMapActivity extends BaseActivity implements OnMapReadyCallback {
     private ShopsModel shop;
+    private GoogleMap map;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        map = mapFragment.getMap();
 
         Intent args = getIntent();
         int id = 0;
@@ -31,4 +45,11 @@ public class ShopMapActivity extends BaseActivity {
         }
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
+
+    }
 }
